32a08cab3dc9ec432bf152f219aa166a6d0e0d68
Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
2017-12-13 18:45:12 +0100

    moved everything

--- templates/create-project-from-template.bat
+++ templates/create-project-from-template.bat
@@ -1,7 +1,7 @@
 @echo off
-IF EXIST ..\application/project (
-	echo Target directory 'project' already exists. Will not copy/overwrite files.
+IF EXIST ..\..\application\project (
+	echo Target directory '..\..\application\project' already exists. Will not copy/overwrite files.
 ) ELSE (
-	xcopy "project-template" "..\application\project" /E /I
+	xcopy "project-template" "..\..\application\project" /E /I
 )
 pause
