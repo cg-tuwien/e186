dcfb77bb9b58117f80a54cf765a295040ed2343d
Lukas Geyer <e1026408@student.tuwien.ac.at>
2018-02-04 20:55:07 +0100

    lights are now passed to the shader in view space to save mat4 multiplication in fragment shader (probably not every occurence in framework replaced) trying to set nonexisting uniforms no longer throws an exception but writes only a debug log -> uniforms that are optimized away do not crash the program (should be useful for debugging)

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -435,7 +435,8 @@ namespace e186
 		auto loc = glGetUniformLocation(m_prog_handle, name.c_str());
 		if (-1 == loc)
 		{
-			throw ExceptionWithCallstack("Uniform '" + name + "' not found.");
+			log_debug("Uniform %s not found.\n", name);
+			//throw ExceptionWithCallstack("Uniform '" + name + "' not found.");
 		}
 		m_uniform_locations[name] = loc;
 		return *this;
