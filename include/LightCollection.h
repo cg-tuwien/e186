0a873e2f573b6f6231805cf7d654c85390dab8a0
Lukas Gersthofer <lukasgersthofer@gmail.com>
2018-03-09 17:01:12 +0100

    added some helpful functions to LightCollection

--- include/LightCollection.h
+++ include/LightCollection.h
@@ -15,14 +15,23 @@ namespace e186
 
 	public:
 		LightCollection() { }
-		LightCollection(int capacity) {
+		LightCollection(int capacity)
+		{
 			m_lights.reserve(capacity);
 			m_lights_gpu.reserve(capacity);
 			m_lights_extract.reserve(capacity);
 		}
-
+		LightCollection(const LightCollection& other) noexcept = default;
+		LightCollection(LightCollection&& other) noexcept = default;
+		LightCollection& operator=(const LightCollection& other) noexcept = default;
+		LightCollection& operator=(LightCollection&& other) noexcept = default;
 		~LightCollection() { }
-		
+
+		L_type& operator [](int i) { return m_lights[i]; }
+		L_type& at(int i) { return m_lights[i]; }
+		const size_t size() const { return m_lights.size(); }
+		const std::vector<L_gpu_type>& gpu_data() const { return m_lights_gpu; }
+
 		void Add(const L_type& light, fn_extract_gpu_data extract_gpu_data)
 		{
 			m_lights.push_back(light);
@@ -30,9 +39,13 @@ namespace e186
 			m_lights_gpu.push_back(extract_gpu_data(light));
 		}
 
-		L_type& operator [](int i)
+		L_type Remove(int i)
 		{
-			return m_lights[i];
+			L_type light = m_lights[i];
+			m_lights.erase(m_lights.begin() + i);
+			m_lights_gpu.erase(m_lights_gpu.begin() + i);
+			m_lights_extract.erase(m_lights_extract.begin() + i);
+			return light;
 		}
 
 		void UpdateGpuData(int i)
@@ -40,20 +53,9 @@ namespace e186
 			m_lights_gpu[i] = m_lights_extract[i](m_lights[i]);
 		}
 
-		const std::vector<L_gpu_type>& GetGpuData() const
-		{
-			return m_lights_gpu;
-		}
-
 		const void* GetGpuDataPtr() const
 		{
 			return &m_lights_gpu[0];
 		}
-
-		const size_t size() const 
-		{
-			return m_lights.size();
-		}
-
 	};
 }
