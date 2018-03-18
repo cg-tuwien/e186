334959414bc62185db5a03a5dcf8a7d978520544
Niko Leopold <niko.leopold@mailbox.org>
2018-03-18 13:50:52 +0100

    instead of using the main entry points of the assignments use src/main.cpp and add scenes for assignments to the ui scene loader menu

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "SceneSelectorScene.h"
 #include "SampleSceneTessIco.h"
 #include "ModelExplorerScene.h"
+#include "../assignment1/A1NormalMapping.h"
 
 int main()
 {
@@ -13,7 +14,8 @@ int main()
 	{
 		auto sel_scene = std::make_unique<e186::SceneSelectorScene>();
 		sel_scene->AddScene<e186::SampleSceneTessIco>();
-		sel_scene->AddSceneGenFunc("Model Explorer: Sponza", []() { return std::make_unique<e186::ModelExplorerScene>("assets/models/sponza/sponza.obj", glm::mat4(1.0f)); });
+		sel_scene->AddSceneGenFunc("Model Explorer: Sponza", []() { return std::make_unique<e186::ModelExplorerScene>("assets/models/sponza/sponza_structure.obj", glm::scale(glm::vec3(.01f, .01f, .01f))); });
+		sel_scene->AddSceneGenFunc("Assignment1", []() { return std::make_unique<artr::A1NormalMapping>(); });
 		return sel_scene;
 	});
 }
