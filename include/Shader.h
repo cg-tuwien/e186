6411d4bebe3cadb2e7e44172ca82c2caa382aeae
Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
2018-03-22 16:59:37 +0100

    added UnbindShader method

--- include/Shader.h
+++ include/Shader.h
@@ -358,4 +358,5 @@ namespace e186
 	void RenderMeshes(const Shader& shader, const MeshRenderData& meshes_and_their_vaos);
 	void RenderMeshesWithAlignedUniformSetters(const Shader& shader, const MeshRenderData& meshes_and_their_vaos, const MeshUniformSettersForShader& uniform_setters);
 	void UnbindVAO();
+	void UnbindShader();
 }
