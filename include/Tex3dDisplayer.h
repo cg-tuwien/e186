17ebfa878ef562a08d2cc93de50403a88fbb3fe2
Niko Leopold <niko.leopold@mailbox.org>
2019-01-17 10:09:22 +0100

    code for pull request voxelization and voxel fill

--- include/Tex3dDisplayer.h
+++ include/Tex3dDisplayer.h
@@ -11,6 +11,7 @@ namespace e186
 		Tex3dDisplayer(Tex3D& tex3d);
 
 		void Render(const glm::mat4& mM, const glm::mat4& vM, const glm::mat4& pM);
+		void Render(const glm::mat4& vM, const glm::mat4& pM); // use AntTweakBar transforms
 
 	private:
 		Tex3D& m_tex3d;
@@ -23,6 +24,9 @@ namespace e186
 		GLsizei m_indices_len;
 
 		glm::vec3 m_element_pos;
+		float m_element_scale;
+		glm::vec3 m_element_rotation_axis;
+		float m_element_rotation_angle;
 		AntTweakBarHandle m_twbar;
 	};
 
